3b5b631b321f086fd55c0b6e214a35e8bc8aa27a
grossmannA <agrossmann315@gmail.com>
2024-01-24 22:33:44 -0500

    updated incorrect Jquery syntax

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ $(document).ready(function () {
   var saveUserInput = function(event) {
     var timeBlock = $(event.target).parent().attr("id");
     var userEventInput = $(event.target).siblings("textarea");
-    localStorage.setItem(timeBlock, userEventInput.value());
-    userEventInput.text = userEventInput.value();
+    localStorage.setItem(timeBlock, userEventInput.val());
+    userEventInput.text = userEventInput.val();
   }
 
-  $("#hour-9 .description").value(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").value(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").value(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").value(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").value(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").value(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").value(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").value(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").value(localStorage.getItem("hour-17"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   $(".saveBtn").on("click", saveUserInput);
 
